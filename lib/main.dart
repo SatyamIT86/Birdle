c6c06b3bbc451d8ed312d31524fd4508ca65f615
Satyam Pandey <satyam.pandey@ctgs.in>
2026-07-27 18:08:06 +0530

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MainApp extends StatelessWidget {
             child: Text('Birdle'),
           ),
         ),
-        body: Center(child: Text('Hello World!')),
+         body: Center(child: GamePage()),
       ),
     );
   }
@@ -52,3 +52,16 @@ class Tile extends StatelessWidget {
     );
   }
 }
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+  // This object is part of the game.dart file.
+  // It manages wordle logic, and is outside the scope of this tutorial.
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Replace with screen contents
+    return Container();
+  }
+}
+
